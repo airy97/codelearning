78a3840e82107248058610ab60a438808f8660af
AiryZ <zhoup97@foxmail.com>
2020-10-29 11:36:00 +0800

    addMFC

--- cpp/cpp/main.cpp
+++ cpp/cpp/main.cpp
@@ -1,9 +1,11 @@
+#include <afxwin.h> 
 #include <iostream>
 
 int main()
 {
 	using namespace std;
-
+	CString str = _T("Hello, World!");
 	cout << "Hello, World!" << endl;
+	wcout << (LPCTSTR)str << endl;
 	return 0;
 }
